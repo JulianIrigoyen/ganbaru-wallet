917193b5e8a4a02bafd422cade5276d9a1655758
Julian Martin Irigoyen <julian.irigoyen@tiendanube.com>
2020-11-30 06:09:02 -0300

    showing doc

--- bundle_documentation.sh
+++ bundle_documentation.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 swagger-cli bundle docs/api_v1.yaml --outfile _build/openapi3.yaml --type yaml
-redoc-cli bundle _build/openapi3.yaml -o "public/docs/index.html"
+redoc-cli bundle _build/openapi3.yaml -o "public/docs/api_v1.html"
 rm -rf _build
-#open public/docs/api_v1.html
+open public/docs/api_v1.html
