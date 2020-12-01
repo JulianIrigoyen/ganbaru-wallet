61d2dea04339ed802980208418643b7dfdf8ea19
Julian Martin Irigoyen <julian.irigoyen@tiendanube.com>
2020-12-01 00:14:14 -0300

    Implemented persistence with reactive mongo and null vector

--- app/adapter/EventAdapter.scala
+++ app/adapter/EventAdapter.scala
@@ -6,8 +6,11 @@ import org.nullvector.{EventAdapter, ReactiveMongoEventSerializer}
 import reactivemongo.api.bson.MacroConfiguration
 import play.Module.WalletsSystem
 import adapter.AdaptableEvents
+import model.WalletFactoryEvents
 import reactivemongo.api.bson.MacroConfiguration.Aux
 import reactivemongo.api.bson.{BSONDocument, MacroConfiguration, MacroOptions, TypeNaming}
+
+/** https://github.com/null-vector/akka-reactivemongo-plugin */
 
 object EventAdapter  {
 
@@ -15,12 +18,10 @@ object EventAdapter  {
 
   def adaptEventsFor(system: ActorSystem) = {
     val serializer = ReactiveMongoEventSerializer(system)
-    Seq[AdaptableEvents](WalletEvents).flatMap(_.adapt()) foreach { a =>  serializer.addEventAdapter(a) }
+    Seq(WalletEvents, WalletFactoryEvents).flatMap(_.adapt()) foreach { a =>  serializer.addEventAdapter(a) }
   }
-    
 }
 
-
 trait AdaptableEvents {
-  def adapt( ): Seq[EventAdapter[_]]
+  def adapt(): Seq[EventAdapter[_]]
 }
--- app/model/WalletFactory.scala
+++ app/model/WalletFactory.scala
@@ -1,5 +1,6 @@
 package model
 
+import adapter.AdaptableEvents
 import akka.Done
 import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 import akka.actor.typed.{ActorRef, Behavior}
@@ -9,6 +10,7 @@ import model.util.Acknowledge
 import model.wallets.Wallet.WalletConfirmation
 import model.wallets.{GanbaruClientId, WalletCommands, WalletId, WalletNumber}
 import model.wallets.WalletCommands.CreateWalletWithNumber
+import org.nullvector.{EventAdapter, EventAdapterFactory}
 import sharding.EntityProvider
 
 object WalletFactory {
@@ -20,47 +22,50 @@ object WalletFactory {
                             replyTo: ActorRef[Acknowledge[WalletId]]
                             ) extends Command
 
-  sealed trait Event
-  case object WalletCreated extends Event
 
   case class WalletNumberState(number: Int)
 
   def apply(ganbaruClientId: GanbaruClientId, walletProvider: EntityProvider[WalletCommands.Command, WalletId]): Behavior[WalletFactory.Command] = {
     Behaviors.setup { implicit context =>
-      EventSourcedBehavior[WalletFactory.Command, Event, WalletNumberState](
+      EventSourcedBehavior[WalletFactory.Command, WalletFactoryEvents.Event, WalletNumberState](
         persistenceId = PersistenceId("WalletNumber", ganbaruClientId.id),
         emptyState = WalletNumberState(222),
         commandHandler = commandHandler(ganbaruClientId, walletProvider),
         eventHandler = eventHandler
-      )
-        .withRetention(RetentionCriteria.snapshotEvery(10, 1).withDeleteEventsOnSnapshot)
+      ).withRetention(RetentionCriteria.snapshotEvery(10, 1).withDeleteEventsOnSnapshot)
     }
   }
 
   private def commandHandler(
                               ganbaruClientId: GanbaruClientId, walletProvider: EntityProvider[WalletCommands.Command, WalletId]
                             )
-                            (implicit context: ActorContext[Command]): (WalletNumberState, Command) => Effect[Event, WalletNumberState] = {
+                            (implicit context: ActorContext[Command]): (WalletNumberState, Command) => Effect[WalletFactoryEvents.Event, WalletNumberState] = {
     (_, command) =>
       command match {
         case ConfirmWallet(confirmation, replyTo) =>
-          Effect.persist(WalletCreated)
-            .thenRun { state =>
+          Effect.persist(WalletFactoryEvents.WalletCreated()).thenRun { state: WalletNumberState =>
               val walletNumber = WalletNumber(state.number)
               val walletId = WalletId.newWalletId(ganbaruClientId)
               walletProvider.entityFor(walletId) ! CreateWalletWithNumber(ganbaruClientId, walletNumber, confirmation, replyTo)
             }
-
       }
   }
 
-  private def eventHandler: (WalletNumberState, Event) => WalletNumberState = { (state, event) =>
+  private def eventHandler: (WalletNumberState, WalletFactoryEvents.Event) => WalletNumberState = { (state, event) =>
     event match {
-      case WalletCreated => state.copy(number = state.number + 1)
+      case WalletFactoryEvents.WalletCreated() => state.copy(number = state.number + 1)
     }
   }
 
+}
 
+object WalletFactoryEvents extends AdaptableEvents {
+  sealed trait Event
 
+  case class WalletCreated() extends Event
 
+  override def adapt(): Seq[EventAdapter[_]] = Seq(
+    EventAdapterFactory.adapt[WalletFactoryEvents.WalletCreated]( "WalletCreated"),
+
+  )
 }
